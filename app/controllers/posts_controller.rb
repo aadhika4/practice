c71327c5b50e09e4a4e268d5a1ea51f074639390
Anjan Adhikari <a_jan2002@hotmail.com>
2012-01-11 10:06:11 -0500

    Adding posts controller, model and view.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    
+    if @post.save
+    else
+      render "new"
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+  
+  def destroy
+
+  end
+end
